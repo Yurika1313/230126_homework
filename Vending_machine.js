6a7bbf09ba23e4155db60b4c3da0405c38e05784
Shiyogo Oji (Woven Alpha)/大路　将吾 <shiyogo.oji@woven-planet.global>
2023-01-26 16:40:19 +0900

    add object by Shogo

--- Vending_machine.js
+++ Vending_machine.js
@@ -1,1 +1,15 @@
-
+let vendingMachine = {
+    cola: {
+        stock: 10,
+        price: 150
+    },
+    tea: {
+        stock: 10,
+        price: 150
+    },
+    coffee: {
+        stock: 0,
+        price: 200
+    }
+};
+console.log(vendingMachine);
