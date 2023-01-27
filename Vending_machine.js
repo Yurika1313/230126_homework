757e8179d38b984cfece6b1495aa321086c5e5e2
Yurika Sawada <yurika.sawada@woven-planet.global>
2023-01-27 10:29:44 +0900

    sawada

--- Vending_machine.js
+++ Vending_machine.js
@@ -20,25 +20,15 @@ function order(yen, label){
     this.yen = yen;
     this.label = label;
       
-         if (label == "cola" && yen >= vendingMachine.cola.price){
-            console.log("You can get cola!");
-            vendingMachine.cola.stock = vendingMachine.cola.stock -1;
-            console.log("We have " + vendingMachine.cola.stock + " left in stock");
-            if(1 > vendingMachine.cola.stock -1){
-                console.log("but...Out of stock,sorry...");
+         if (yen >= vendingMachine[label].price){
+            if(0 < vendingMachine[label].stock ){
+                console.log("You can get " + this.label + "!");
+            vendingMachine[label].stock = vendingMachine[label].stock -1;
+            console.log("We have " + vendingMachine[label].stock + " left in stock");
                 }
-         }else if(label == "tea" && yen >= vendingMachine.tea.price ){
-            console.log("You can get tea!");
-            vendingMachine.tea.stock = vendingMachine.tea.stock -1;
-            console.log("We have " + vendingMachine.tea.stock + " left in stock");
-            if(1 > vendingMachine.tea.stock -1){
-                console.log("but...Out of stock,sorry...");
+            else if(0 == vendingMachine[label].stock ){
+                console.log("Out of stock,sorry...");
                 }
-         }else if(label === "coffee" && yen >= vendingMachine.coffee.price ){
-            console.log("You have money to get coffee!");
-            if(1 > vendingMachine.coffee.stock -1){
-            console.log("but...Out of stock,sorry...");
-            }
          }else{
             console.log("You cannot get it... Prease put more money.")
          }
@@ -46,13 +36,13 @@ function order(yen, label){
 
     order.prototype = {
         getData : function() {
-          return "The money I put in is [" + this.yen + "]; The drink of choice is [" + this.label + "].";
+          return "The money you put in is [" + this.yen + "]; The drink of choice is [" + this.label + "].";
         }
       };
 
 //Mike：order(入れるお金(yen)、欲しい飲み物(label))を入力してください。
 console.log("*Mike*");
-var Mike = new order(200,"tea");
+var Mike = new order(200,"cola");
     console.log(Mike.getData());
 
 //Anna：order(入れるお金(yen)、欲しい飲み物(label))を入力してください。
@@ -60,8 +50,15 @@ console.log("*Anna*");
 var Anna = new order(200,"coffee");
 console.log(Anna.getData());
 
+//Tom：order(入れるお金(yen)、欲しい飲み物(label))を入力してください。
+console.log("*Tom*");
+var Tom = new order(100,"tea");
+console.log(Tom.getData());
+
 
 console.log("The type of drink is [" + Object.keys(vendingMachine) + "]."); 
+
+
 
 //console.log(vendingMachine);
 
